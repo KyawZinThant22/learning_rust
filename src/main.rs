7457cdb995661fccf8c38db7fcf3cdf2e2835e65
KyawZinThant22 <kyawthantver2@gmail.com>
2022-11-28 23:46:40 +0630

    function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello Rust");
+    let sum = my_function(3, 5);
+    println!("Sum of the number is : {}", sum);
+}
+
+fn my_function(x: i32, y: i32) -> i32 {
+    println!("The value of x is :{} ", x);
+    println!("The value of y is :{}", y);
+    let sum: i32 = x + y;
+    return sum;
 }
