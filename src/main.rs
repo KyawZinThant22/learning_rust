d711a3d52bd3e0df47c091f70f5af03a96f0d17a
KyawZinThant22 <kyawthantver2@gmail.com>
2022-11-28 23:37:17 +0630

    data types

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
 fn main() {
-    let mut x = 5;
-    println!("The Value of x is : {}", x);
-    x = 4;
-    println!("The value of x {}", x);
+    //scalar data types and compount dataTypes
+    //scalar data types represents single value
+    //compount data types represents group of value
 
-    const SKILLED_LANGUAGE_COUNT: u32 = 3;
+    // rust have fout main scalar data types
+    //Integers  singes integers could be postive or negative numbers and unsinges numbers can only be positive number
+    //floating point numbers
+    //Boolean
+    //Character
+
+    //compound types
+
+    let tup = ("Let's Learn Rust", 3);
+    let (learning, count) = tup;
+    let language_count = tup.1;
+
+    let err_codes = [212, 123, 133];
+
+    // let not_found = err_codes[5];
+
+    //you can also declare array like this
+    let byte = [0; 8];
 }
