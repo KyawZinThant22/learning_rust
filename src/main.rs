39c6c465a738b88b949ff1b3d9c6a935bdaa34c2
KyawZinThant22 <kyawthantver2@gmail.com>
2022-11-28 22:36:03 +0630

    basic variable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello Rust");
+    let mut x = 5;
+    println!("The Value of x is : {}", x);
+    x = 4;
+    println!("The value of x {}", x);
+
+    const SKILLED_LANGUAGE_COUNT: u32 = 3;
 }
