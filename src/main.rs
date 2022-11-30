9e9cc4643c0809414de6ac11ebdf43910f088e8a
KyawZinThant22 <kyawthantver2@gmail.com>
2022-11-30 22:29:19 +0630

    loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
-    println!("Hello Rust");
+    println!("Hello loop in rust");
+
+    // let result = loop {
+    //     counter += 1;
+
+    //     if counter == 10 {
+    //         break counter;
+    //     }
+    // };
+    // println!("The result is {}", result);
+
+    // while number != 0 {
+    //     println!("{}!", number);
+
+    //     number -= 1;
+    // }
+
+    // println!("Loop stop")
+
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a.iter() {
+        println!("The value is {}", element)
+    }
+
+    for number in 1..11 {
+        println!("{}!", number)
+    }
 }
