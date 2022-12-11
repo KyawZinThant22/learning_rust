0c29a9a47ad14226b60f30d05ae722dca1cd19ed
KyawZinThant22 <kyawthantver2@gmail.com>
2022-12-11 14:52:39 +0630

    owner ship

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello Rust");
+    // ---- OwnerShip rules -----
+    // 1. Each value in Rust has a variable that's called its owner.
+    // 2. There can only be one owner at a time .
+    // 3. When the owner goes out of scope , the value will be dropped.
+
+    let x = 5;
+    let y = x;
+
+    println!("The value of y is : {y}");
 }
