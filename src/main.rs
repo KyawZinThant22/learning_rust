facac3edf489bfdf032a2745db04ef31a9506713
KyawZinThant22 <kyawthantver2@gmail.com>
2022-12-11 16:01:59 +0630

    :owndership

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,32 @@ fn main() {
     // 2. There can only be one owner at a time .
     // 3. When the owner goes out of scope , the value will be dropped.
 
-    let x = 5;
-    let y = x;
 
-    println!("The value of y is : {y}");
+    // let mut s = String::from("hello");
+
+    // s.push_str(", world!");
+
+    // println!("{}",s);
+
+    // let s1 = String::from("Hello");
+    // let s2 = s1.clone();
+
+    // println!("{} , world" , s1);
+
+    let s = String::from("Hello");
+    takes_ownerwhip(s);
+    let x = 5;
+
+    makes_integer(x);
+
+
+}
+
+
+fn takes_ownerwhip(some_string : String){
+    println!("{}",some_string);
+}
+
+fn makes_integer(some_integer : i32) {
+    println!("{}", some_integer);
 }
