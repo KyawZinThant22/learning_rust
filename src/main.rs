3d84059d556fd2e4077b00b6b223ea0b70e682ee
KyawZinThant22 <kyawthantver2@gmail.com>
2022-12-11 19:07:48 +0630

    :owndership

--- src/main.rs
+++ src/main.rs
@@ -16,20 +16,54 @@ fn main() {
 
     // println!("{} , world" , s1);
 
-    let s = String::from("Hello");
-    takes_ownerwhip(s);
-    let x = 5;
+//     let s = String::from("Hello");
+//     takes_ownerwhip(s);
+//     let x = 5;
 
-    makes_integer(x);
+//     makes_integer(x);
 
+
+// }
+
+
+// fn takes_ownerwhip(some_string : String){
+//     println!("{}",some_string);
+// }
+
+// fn makes_integer(some_integer : i32) {
+//     println!("{}", some_integer);
+// }
+
+
+// let s1 = give_owership();
+// let s2 = String::from("World");
+// let s3 = take_and_gives_back(s2);
+// println!("s1={} , s3={}", s1,s3);
+
+// }
+
+// fn give_owership() -> String {
+//     let some_string = String::from("Yours");
+//     some_string
+// }
+
+// fn take_and_gives_back (some_string : String) ->String {
+//     some_string
+// }
+
+// Rust does let us return multiple values using a tuple
+
+let s1 = String::from("Hello");
+
+let (s2 , len) = calculate_length(s1);
+
+println!("s3={} , len={}" , s2,len);
 
 }
 
 
-fn takes_ownerwhip(some_string : String){
-    println!("{}",some_string);
-}
+fn calculate_length  (str : String) -> (String,usize) {
+    let length = str.len();
 
-fn makes_integer(some_integer : i32) {
-    println!("{}", some_integer);
+    (str,length)
 }
